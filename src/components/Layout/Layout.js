eb1ee00cfe3accab35963a657af771b422a386d2
Siddharth Nasare <snasare@MRK-22753.local>
2020-04-11 19:36:22 -0400

    Added BurgerBuilder container and Layout component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,9 @@ const layout = ( props ) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
         <main>
-            {props.children}
+            { props.children }
         </main>
     </Aux>
 );
+
+export default layout;
