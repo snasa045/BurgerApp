05e9953d48eee4c652585584a06075320ae75810
Siddharth Nasare <snasare@MRK-22753.local>
2020-04-12 16:16:03 -0400

    Burger ingredients added with loop

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,12 +4,36 @@ import "./Burger.scss";
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
-const burger = () => {
+const burger = (props) => {
+
+    let transformedIngredients = Object.keys(props.ingredients)
+                                    .map(igKey => {
+
+                                        // returns the array of ingredients keys
+                                        // console.log(Object.keys(props.ingredients));
+
+                                        // returns the array of ingredients individual key's value length 
+                                        // const arrayI = [...Array(props.ingredients[igKey])]
+                                        // console.log(arrayI);
+                                        return [...Array(props.ingredients[igKey])]
+                                            .map((_, i) => {
+                                                return <BurgerIngredient key={igKey + i} type={igKey}/>
+                                            });
+                                    })
+                                    .reduce((arr, el) => {
+                                        return arr.concat(el);
+                                    }, []);
+
+    console.log(transformedIngredients);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
     return (
         <div className = "Burger">
             <BurgerIngredient type="bread-top"/>
-            <BurgerIngredient type="cheese"/>
-            <BurgerIngredient type="meat"/>
+            {transformedIngredients}
             <BurgerIngredient type="bread-bottom"/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,10 +5,20 @@ import Aux from "../../hoc/Aux";
 import Burger from '../../components/Burger/Burger'
 
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    };
+
     render() {
         return (
             <Aux>
-                <Burger/>
+                <Burger ingredients = {this.state.ingredients}/>
                 <div>Burger controls</div>
             </Aux>
         )
